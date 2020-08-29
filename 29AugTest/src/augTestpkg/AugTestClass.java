36f8c1f760b697f34f72d00870009632b8ceaa1f
Mridul Sharma <mridul@mpurse.co.in>
2020-08-29 10:16:40 +0530

    29AugTest

---
+++ 29AugTest/src/augTestpkg/AugTestClass.java
@@ -0,0 +1,10 @@
+package augTestpkg;
+
+public class AugTestClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+System.out.println("Aug Test Calss ");
+	}
+
+}
